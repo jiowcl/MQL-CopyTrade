06fc59b2e919234aa5c28717ef88db8017ff05f4
Jiowcl <jiowcl@gmail.com>
2019-05-07 15:52:26 +0800

    Add files via upload

---
+++ Example/Java/src/main/java/test/App.java
@@ -0,0 +1,50 @@
+package test;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+/**
+ * Test
+ */
+public final class App {
+    private App() {
+        
+    }
+
+    /**
+     * Main.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        ZContext context = new ZContext();
+        ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
+
+        Boolean enabled = true;
+        String serverAddr = "tcp://localhost:5559";
+
+        subscriber.connect(serverAddr);
+        subscriber.subscribe("");
+
+        while (enabled) {
+            String messageReceived = subscriber.recvStr(0).trim();
+            String[] messageData = messageReceived.split(" ");
+
+            if (messageData.length != 2)
+                continue;
+
+            String[] orderData = messageData[1].toString().split("\\|");
+
+            if (orderData.length != 9)
+                continue;
+
+            System.out.println(messageData[1]);
+            System.out.println("Login: " + messageData[0] + ", Action: " + orderData[0] + ", Symbol: " + orderData[1]);
+        }
+
+        subscriber.disconnect(serverAddr);
+        subscriber.close();
+
+        context.close();
+    }
+}
---
+++ Example/Java/src/test/java/test/AppTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
